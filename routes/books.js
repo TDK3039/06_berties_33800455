551b004748d3a9f273d663f9b946097f25ab15ba
Drajah223 <davidrajah325@outlook.com>
2025-11-13 19:47:57 +0000

    made changes tand completed extension

--- routes/books.js
+++ routes/books.js
@@ -1,14 +1,23 @@
 // Create a new router
-const express = require("express")
+const express = require("express");
+const { search } = require("./main");
 const router = express.Router()
 
 router.get('/search',function(req, res, next){
     res.render("search.ejs")
 });
 
-router.get('/search-result', function (req, res, next) {
+router.get('/search_result', function (req, res, next) {
     //searching in the database
-    res.send("You searched for: " + req.query.keyword)
+    let keyword = req.query.search_text;
+    let sqlquery = "SELECT * FROM books WHERE name LIKE ?";
+    db.query(sqlquery, ['%' + keyword + '%'], (err, result) => {
+        if(err){
+            next(err);
+        } else{
+            res.render("searchresult.ejs", { searchResults: result, keyword: keyword});
+        };
+    });
 });
 
 router.get('/list', function(req, res, next) {
@@ -17,7 +26,36 @@ router.get('/list', function(req, res, next) {
         if (err) {
             next(err); // pass error to Express error handler
         } else {
-            res.send(result); // send book data as JSON
+            res.render("list.ejs", { availableBooks: result});
+        }
+    });
+});
+
+//Bargaining the books
+router.get('/bargainbooks', function(req, res, next) {
+    let sqlquery = "SELECT * FROM books WHERE price < 20";
+    db.query(sqlquery, (err, result) => {
+        if (err) {
+            next(err);
+        } else {
+            res.render("bargainbooks.ejs", { bargainBooks: result });
+        }
+    });
+});
+
+//Adding books
+router.get('/addbook', function (req, res, next) {
+    res.render('addbook.ejs');
+});
+
+router.post('/bookadded', function (req, res, next) {
+    let sqlquery = "INSERT INTO books (name, price) VALUES (?,?)";
+    let newrecord = [req.body.name, req.body.price];
+    db.query(sqlquery, newrecord, (err, result) => {
+        if (err) {
+            next(err);
+        } else {
+            res.send('This book is added to database, name: ' + req.body.name + ', price: £' + req.body.price);
         }
     });
 });
